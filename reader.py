1063e8de440aa808ab8452f67445752e7761bc43
Aleksander Kasprzyk <apropolos@gmail.com>
2023-09-21 16:13:20 +0200

    Poprawione :).

--- reader.py
+++ reader.py
@@ -2,27 +2,32 @@ import sys
 
 input_file, output_file, variables = sys.argv[1], sys.argv[2], sys.argv[3:]
 
-operation_list = []
-with open('input.csv', mode='r+') as file_stream:
+user_input = sys.argv[3:]
+if not user_input:
+    print("Error: Value is required.")
 
-    input_date = file_stream.read()
-    input_date = input_date.split("\n")
-    for element in input_date:
-        element = element.split(",")
-        operation_list.append(element)
+elif user_input:
+    operation_list = []
+    with open('input.csv', mode='r+') as file_stream:
 
-
-for operation in variables:
-    row, column, value = operation.split(",")
-    row = int(row)
-    column = int(column)
-    operation_list[row][column] = value
+        input_date = file_stream.read()
+        input_date = input_date.split("\n")
+        for element in input_date:
+            element = element.split(",")
+            operation_list.append(element)
 
 
-with open('output.csv', mode="w") as file_stream:
-    output_file = file_stream.write(f"{input_date} \n")
-    for line in operation_list:
-        file_stream.write(",".join(line) + "\n")
-        print(",".join(line))
+    for operation in variables:
+        row, column, value = operation.split(",")
+        row = int(row)
+        column = int(column)
+        operation_list[row][column] = value
+
+
+    with open('output.csv', mode="w") as file_stream:
+        #output_file = file_stream.write(f"{input_date} \n")
+        for line in operation_list:
+            file_stream.write(",".join(line) + "\n")
+            print(",".join(line))
 
 #python reader.py in.csv out.csv 0,0,gitara 3,1,kubek 1,2,17 3,3,0
